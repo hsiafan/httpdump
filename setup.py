ce17134fa16618f7771d007d477b8aea0aa60ba0
090h <moscow@defcon.su>
2015-09-02 22:19:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import codecs
 from setuptools import setup
 
 
-with codecs.open('README.rst', encoding='utf-8') as f:
+with codecs.open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
